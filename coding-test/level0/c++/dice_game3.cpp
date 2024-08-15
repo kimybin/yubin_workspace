01c6004bc0a8743dd3103b8a8b6558cf0c43818b
kimybin <kybsophie0504@gmail.com>
2024-08-16 01:19:48 +0900

    Create dice_game3.cpp

---
+++ coding-test/level0/c++/dice_game3.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int solution(int a, int b, int c, int d) {
+    int answer = 0;
+    
+     if(a == b && a == c && c == d)
+        answer = 1111*a;
+    
+    else if(a != b && b == c && c == d)
+        answer = (10*b+a)*(10*b+a);
+    else if(b != a && a == c && c == d)
+        answer = (10*a+b)*(10*a+b);
+    else if(c != a && a == b && a == d)
+        answer = (10*a+c)*(10*a+c);
+    else if(d != a && a == b && a == c)
+        answer = (10*a+d)*(10*a+d);
+        
+    else if(a == b && c == d && a != c)
+        answer = (a+c) * abs(a-c);
+    else if(a == c && b == d && a != b)
+        answer = (a+b) * abs(a-b);
+    else if(a == d && b == c && a != b)
+        answer = (a+b) * abs(a-b);
+                          
+    else if(a == b && a != c && a != d && c != d)
+        answer = c * d;
+    else if(a == c && a != b && a != d && b != d)
+        answer = b * d;
+    else if(a == d && a != b && a != c && b != c)
+        answer = b * c;
+    else if(b == c && b != a && b != d && a != d)
+        answer = a * d;
+    else if(b == d && b != a && b != c && a != c)
+        answer = a * c;
+    else if(c == d && c != a && c != b && a != b)
+        answer = a * b;
+                  
+    else if(a != b && a != c && c != d)
+        answer = min(min(a, b), min(c, d));
+    
+    return answer;
+}
+
+int main()
+{
+    int a, b, c, d;
+    cout << "Enter a, b, c and d: ";
+    cin >> a >> b >> c >> d;
+
+    int result = solution(a, b, c, d);
+    cout << "The result is: " << result << endl;
+    return 0;
+}
