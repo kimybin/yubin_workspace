4ab35d1a375022cba067cf551d3d1940ee29edd3
kimybin <kybsophie0504@gmail.com>
2025-01-06 21:27:31 +0900

    Update re_time.cpp

--- coding-test/level0/c++/re_time.cpp
+++ coding-test/level0/c++/re_time.cpp
@@ -5,9 +5,7 @@
 
 using namespace std;
 
-string solution(string my_string, vector<vector<int> > queries) {
-    string answer = "";
-    
+string solution(string my_string, vector<vector<int> > queries) {   
     for(int i = 0; i < queries.size(); i++)
         reverse(my_string.begin() + queries[i][0], my_string.begin() + queries[i][1] + 1);
     
